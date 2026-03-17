b2cc7b5bd1399bd2a9251c607fe77c0130a9d7ef
nozwock <57829219+nozwock@users.noreply.github.com>
2026-03-17 22:41:35 +0530

    Add option to disable Ardor loss on death
    Disabled by default. Only the 100% Ardor recover patch is not optional
    with the mod.

--- NoArdorLoss/Plugin.cs
+++ NoArdorLoss/Plugin.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -14,20 +16,36 @@ public partial class Plugin : BaseUnityPlugin
     internal static new ManualLogSource Logger;
     static Harmony harmony;
 
+    const string configCategoryGeneral = "General";
+    static ConfigEntry<bool> configNoArdorLossOnDeath;
+
     void Awake()
     {
         Logger = base.Logger;
 
+        configNoArdorLossOnDeath = Config.Bind(
+            configCategoryGeneral,
+            "NoArdorLossOnDeath",
+            false,
+            "Requires restart to take effect.");
+
         Logger.LogInfo($"Plugin {Id} is loaded!");
 
-        harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
-        foreach (var method in harmony.GetPatchedMethods())
+        try
         {
-            Logger.LogInfo($"Patched method: {method.DeclaringType.FullName}.{method.Name}");
+            harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+            foreach (var method in harmony.GetPatchedMethods())
+            {
+                Logger.LogInfo($"Patched method: {method.DeclaringType.FullName}.{method.Name}");
+            }
+            if (harmony.GetPatchedMethods().Count() == 0)
+            {
+                Logger.LogError($"Failed to apply Harmony patches.");
+            }
         }
-        if (harmony.GetPatchedMethods().Count() == 0)
+        catch (Exception ex)
         {
-            Logger.LogError($"Failed to apply Harmony patches.");
+            Logger.LogError($"Harmony patching failed: {ex}");
         }
     }
 
@@ -65,6 +83,57 @@ public partial class Plugin : BaseUnityPlugin
                     // Always return true
                     yield return new CodeInstruction(OpCodes.Pop);
                     yield return new CodeInstruction(OpCodes.Ldc_I4_1);
+                }
+            }
+        }
+
+        [HarmonyPatch(
+            typeof(Character_CustomModule_PlayerController),
+            nameof(Character_CustomModule_PlayerController.OnPlayerDied))]
+        class Patch_Character_CustomModule_PlayerController_OnPlayerDied
+        {
+            static bool Prepare()
+            {
+                return configNoArdorLossOnDeath.Value;
+            }
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var justMatchArdorZeroed = false;
+
+                var codes = instructions.ToArray();
+                for (var i = 0; i < codes.Length; i++)
+                {
+                    justMatchArdorZeroed = false;
+
+                    // Prevent ardor from being reset to 0
+                    // NOTE: Not doing Nops because of jump label being messed (isn't Harmony supposed to handle that?
+                    // Maybe it's because this is older BepInEx/Harmony?)
+                    if (codes[i].Calls(AccessTools.PropertySetter(
+                            typeof(Character_CustomModule_PlayerAttributesHandler),
+                            nameof(Character_CustomModule_PlayerAttributesHandler.ardor))))
+                    {
+                        justMatchArdorZeroed = true;
+
+                        Logger.LogDebug("Altering instruction `attributesHandler.ardor = 0`"
+                            + " -> `attributesHandler.ardor = attributesHandler.ardor`");
+                        yield return new CodeInstruction(OpCodes.Pop);
+                        yield return Transpilers.EmitDelegate(() =>
+                        {
+                            return Character_CustomModule_PlayerController.instance.attributesHandler.ardor;
+                        });
+                    }
+
+                    yield return codes[i];
+
+                    if (justMatchArdorZeroed)
+                    {
+                        Logger.LogDebug("Setting `.deadVessel_ardorAmount = 0`");
+                        yield return Transpilers.EmitDelegate(() =>
+                        {
+                            SyncHandler.getGeneralData.deadVessel_ardorAmount = 0;
+                        });
+                    }
                 }
             }
         }
--- NoArdorLoss/README.md
+++ NoArdorLoss/README.md
@@ -1,3 +1,7 @@
 # No Ardor Loss
 
 Always restore all the Ardor on destroying "Lost Vessel", similar to the "Shell Lurker" trait.
+
+Optionally, disable Ardor on death.
+
+Ardor is Mass (currency) gain multiplier first and foremost, but it's also used as a factor in various Traits.
