124c067ffb1855ed4dbcf03d04eb38cce1849ac9
nozwock <57829219+nozwock@users.noreply.github.com>
2026-03-21 12:37:33 +0530

    Add AlwaysOnWarp mod

---
+++ AlwaysOnWarp/Plugin.cs
@@ -0,0 +1,69 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AlwaysOnWarp;
+
+[BepInAutoPlugin(id: "nozwock.AlwaysOnWarp")]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    static Harmony harmony;
+
+    void Awake()
+    {
+        Logger = base.Logger;
+
+        Logger.LogInfo($"Plugin {Id} is loaded!");
+
+        try
+        {
+            harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+            foreach (var method in harmony.GetPatchedMethods())
+            {
+                Logger.LogInfo($"Patched method: {method.DeclaringType.FullName}.{method.Name}");
+            }
+            if (harmony.GetPatchedMethods().Count() == 0)
+            {
+                Logger.LogError($"Failed to apply Harmony patches.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError($"Harmony patching failed: {ex}");
+        }
+    }
+
+    // void OnDestroy()
+    // {
+    //     Logger.LogInfo($"Unloading plugin {Id}");
+    //     harmony?.UnpatchSelf();
+    // }
+
+    [HarmonyPatch]
+    class Patches
+    {
+        [HarmonyPatch(typeof(GUI_CheckpointOptionsMenu), nameof(GUI_CheckpointOptionsMenu.OpenMenu))]
+        class Patch_SetActiveTeleport
+        {
+            static void Prefix(GUI_CheckpointOptionsMenu __instance, out bool __state)
+            {
+                __state = __instance.isActive;
+            }
+
+            static void Postfix(GUI_CheckpointOptionsMenu __instance, bool __state)
+            {
+                if (__state) return;
+
+                __instance.panelButtons
+                    .FirstOrDefault(it => it.name.IndexOf("Teleport", StringComparison.OrdinalIgnoreCase) >= 0)
+                    ?.gameObject
+                    .SetActive(true);
+            }
+        }
+    }
+}
+
---
+++ AlwaysOnWarp/README.md
@@ -0,0 +1,4 @@
+# Always On Warp
+
+Makes the Warp ability available from the very start of the game, which lets the player teleport (warp) between save
+points (surrogates).
